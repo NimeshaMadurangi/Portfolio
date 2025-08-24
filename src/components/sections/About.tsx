6de02e0efa574fa749076d594e09291711fdaedb
nimesha8 <maduranginimesha8@gmail.com>
2025-08-24 22:21:27 +0530

    change images

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -9,7 +9,7 @@ const About: React.FC = () => {
             <div className="relative">
               <div className="w-64 h-64 md:w-80 md:h-80 rounded-full overflow-hidden border-4 border-white dark:border-gray-700 shadow-xl">
                 <img
-                  src="../public/nimesha.jpg"
+                  src="/nimesha.jpg"
                   alt="Nimesha Madurangi"
                   className="w-full h-full object-cover"
                 />
